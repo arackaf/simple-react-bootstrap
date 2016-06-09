fb5697b43a1deafdb4b87f452812048fa8831b6f
Adam Rackis <adamrackis@gmail.com>
2016-06-09 00:06:52 -0500

    Poking around still

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+document.onclick = function(evt){
+    debugger;
+};
+
 class NavBar extends React.Component{
     render(){
-        return <div>Hello world!!!!!!</div>
+        let children = this.props.children;
+        let count = children.filter(c => c.type === NavBarItem).length;
+
+        alert(count);
+
+        return <div>Hello world!!!!!! <button className="____internal">Click me</button></div>
     }
 }
+
+class NavBarItem extends React.Component {
+    render(){
+        return <div>Item</div>
+    }
+}
+
+NavBar.Item = NavBarItem;
 
 export default NavBar;
--- src/testIt.js
+++ src/testIt.js
@@ -2,6 +2,13 @@ import NavBar from './NavBar.js';
 import { render } from 'react-dom';
 import React from 'react';
 
-render(<NavBar></NavBar>, document.getElementById('home'));
+render(
+    <NavBar>
+        <div>Div 1 <button>btn</button></div>
+        <NavBar.Item></NavBar.Item>
+        <div>Div 2</div>
+        <div>Div 3 <div>foo <span>s</span></div></div>
+        <NavBar.Item></NavBar.Item>
+    </NavBar>, document.getElementById('home'));
 
 export default null;
